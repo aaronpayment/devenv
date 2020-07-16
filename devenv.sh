b3a192848680557b4f928fdea620064e9a430ad4
aaronpayment <aaronpayment@users.noreply.github.com>
2020-07-15 21:41:30 -0700

    Initial devenv files

---
+++ devenv.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+install() {
+    echo "--- Install ---"
+    local devenvbase="$(cd $(dirname ${BASH_SOURCE}) && pwd)"
+    local dotfiles="${devenvbase}/dotfiles/*"
+    local bin="${devenvbase}/bin"
+
+    local backupdir="${HOME}/devenv_bak"
+    mkdir -p "${backupdir}"
+
+    # === dotfiles ===
+    GLOBIGNORE=".:.."
+    local file
+    for file in ${dotfiles} ${bin}; do
+	[[ -f "${file}" || -d "${file}" ]] || continue
+
+	local homefile="${HOME}/$(basename ${file})"
+	echo "homefile: ${homefile}"
+
+	# contine if installed ( symlinked )
+	[[ -L "${homefile}" ]] && continue
+
+	# backup if exists
+	if [[ -f "${homefile}" || -d "${homefile}" ]]; then
+	    echo "  BACKUP:  ${homefile}"
+	    echo "   mv" "${homefile}" "${backupdir}/"
+	    mv "${homefile}" "${backupdir}/"
+	fi
+
+	#install 
+	echo "  INSTALL: ${homefile}"
+	echo "   ln -s" "${file}" "${homefile})"
+	ln -s "${file}" "${homefile}"
+    done
+
+    source ${HOME}/.bash_profile
+}
+
+usage() {
+    cat <<EOF
+
+SYNOPSIS
+   devenv [OPTIONS]
+
+OPTIONS
+   -i|--install   Install devenv
+   -h|--help      Help
+EOF
+}
+
+while [ "${1}" != "" ]; do
+    case $1 in
+	-h | --help)
+	    usage
+	    ;;
+	-i | --install)
+	    install
+	    ;;
+	*)
+	    ;;
+    esac
+    shift
+done
+
+unset install
+unset usage
